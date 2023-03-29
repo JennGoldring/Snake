5ca25c967497c6f1e14876b5695212926a5bef89
JennGoldring <jenngoldring@yahoo.com>
2023-03-29 18:23:02 -0500

    Building Game Panel

--- GamePanel.java
+++ GamePanel.java
@@ -26,6 +26,7 @@ public class GamePanel extends JPanel implements ActionListener {
         this.setBackground(Color.black);
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
+        startGame();
 
     }
     public void startGame() {
