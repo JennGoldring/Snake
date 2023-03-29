5682cc1f5da65ac36471eb21c8c4d6fe2cc27bdb
JennGoldring <jenngoldring@yahoo.com>
2023-03-29 18:07:35 -0500

    Initial steps for game

---
+++ GamePanel.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.swing.*;
+import java.util.Random;
+import javax.swing.Jpanel;
+public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel(){
+    }
+    public void startGame() {
+
+    }
+    public void paintComponent(Graphics g) {
+
+    }
+    public void move() {
+
+    }
+    public void checkApple() {
+
+    }
+    public void checkCollisions() {
+
+    }
+    public void gameOver(Graphics g){
+
+    }
+    public class MyKeyAdapter extends KeyAdapter {
+
+
+    }
+}
